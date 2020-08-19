8766badf72903f325ec84ccc2daa7f95f5e0d594
llach <llach@techfak.uni-bielefeld.de>
2020-08-19 20:07:32 +0200

    ROS node for ta11

---
+++ src/load_cell_publisher.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import u6
+import rospy
+
+import numpy as np
+
+from _collections import deque
+from tiago_tactile.msg import TA11
+
+rospy.init_node('ta11_tactile', anonymous=True)
+pub = rospy.Publisher('tactile/load_cell', TA11, queue_size=1)
+
+numChannels = 2
+
+SCALING = rospy.get_param("~scaling")
+SMOOTHING = rospy.get_param("~smoothing")
+resolutionIndex = rospy.get_param("~resolutionIndex")
+gainIndex = rospy.get_param("~gainIndex")
+settlingFactor = rospy.get_param("~settlingFactor")
+differential = rospy.get_param("~differential")
+
+latest_values = [deque(maxlen=SMOOTHING) for _ in range(numChannels)]
+
+d = u6.U6()
+d.getCalibrationData()
+
+try:
+    # Configure the IOs before the test starts
+    rospy.loginfo("Preparing LabJack U6 ...")
+    FIOEIOAnalog = (2 ** numChannels) - 1
+    fios = FIOEIOAnalog & 0xFF
+    eios = FIOEIOAnalog // 256
+
+    d.getFeedback(u6.PortDirWrite(Direction=[0, 0, 0], WriteMask=[0, 0, 15]))
+
+    feedbackArguments = []
+
+    feedbackArguments.append(u6.DAC0_8(Value=125))
+    feedbackArguments.append(u6.PortStateRead())
+
+    feedbackArguments.append(u6.AIN24(0, resolutionIndex, gainIndex, settlingFactor, differential))
+    feedbackArguments.append(u6.AIN24(2, resolutionIndex, gainIndex, settlingFactor, differential))
+
+    rospy.loginfo("Publishing sensor values!")
+    while not rospy.is_shutdown():
+        results = d.getFeedback(feedbackArguments)
+
+        for j in range(numChannels):
+            latest_values[j].append(d.binaryToCalibratedAnalogVoltage(gainIndex, results[2 + j]) * SCALING)
+
+        m = TA11()
+        m.header.stamp = rospy.Time.now()
+        m.sensor_values = [np.mean(dq) for dq in latest_values]
+
+        pub.publish(m)
+finally:
+    d.close()
---
+++ src/measure.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+
+import u6
+import rospy
+import signal
+import argparse
+
+import numpy as np
+
+ITERATIONS = 1000
+
+rospy.init_node('ta11_tactile', anonymous=True)
+
+SCALING = rospy.get_param("~scaling")
+SMOOTHING = rospy.get_param("~smoothing")
+resolutionIndex = rospy.get_param("~resolutionIndex")
+gainIndex = rospy.get_param("~gainIndex")
+settlingFactor = rospy.get_param("~settlingFactor")
+differential = rospy.get_param("~differential")
+
+right_m = rospy.get_param("~right_m")
+right_b = rospy.get_param("~right_b")
+
+left_m = rospy.get_param("~left_m")
+left_b = rospy.get_param("~left_b")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--calibrate', default=0, type=int)
+parser.add_argument('-s', '--sensor', default=1, type=int)
+
+args, _ = parser.parse_known_args()
+
+# sensor 1 (default) is connected to AIN0, sensor 2 is connected to AIN2
+sensor = 2 if args.sensor == 2 else 0
+cal = False if args.calibrate == 0 else True
+if sensor == 2:
+    _m = left_m
+    _b = left_b
+else:
+    _m = right_m
+    _b = right_b
+
+print(
+    "\n~~~ TA11 Sensor Measurement ~~~\n\n" +
+    "Parameters:\n" +
+    "Scaling:\t {}\n".format(SCALING) +
+    "Smmothing:\t {}\n".format(SMOOTHING) +
+    "ResIndex\t {}\n".format(resolutionIndex) +
+    "Gain:\t\t {}\n".format(gainIndex) +
+    "Settling:\t {}\n".format(settlingFactor) +
+    "\n" +
+    "Sensor Config:\n"
+    "Sensor:\t\t {}\n".format("LEFT (2)" if sensor == 2 else "RIGHT (1)") +
+    "m:\t\t {}\n".format(_m) +
+    "b:\t\t {}\n\n".format(_b) +
+    "Values are {}CALIBRATED\n".format("NOT " if not cal else "")
+)
+
+
+
+d = u6.U6()
+d.getCalibrationData()
+
+
+
+def signal_handler(signal, frame):
+  print("Quitting.")
+  d.close()
+  exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+try:
+    # Configure the IOs before the test starts
+    rospy.loginfo("Preparing LabJack U6 ...")
+    FIOEIOAnalog = 1
+    fios = FIOEIOAnalog & 0xFF
+    eios = FIOEIOAnalog // 256
+
+    d.getFeedback(u6.PortDirWrite(Direction=[0, 0, 0], WriteMask=[0, 0, 15]))
+
+    feedbackArguments = []
+
+    feedbackArguments.append(u6.DAC0_8(Value=125))
+    feedbackArguments.append(u6.PortStateRead())
+
+    feedbackArguments.append(u6.AIN24(sensor, resolutionIndex, gainIndex, settlingFactor, differential))
+
+    while True:
+        latest_values = []
+        try:
+            raw_input("Press ENTER to start the measurement")
+        except KeyboardInterrupt:
+            print("Ok ok, quitting")
+            exit(0)
+
+        print("Collecting data ...")
+        for _ in range(ITERATIONS):
+            results = d.getFeedback(feedbackArguments)
+            latest_values.append(d.binaryToCalibratedAnalogVoltage(gainIndex, results[2]) * SCALING)
+
+        res = np.mean(latest_values)
+        if cal:
+            res = int(_m * (res + _b))
+        print("Result ({}CALIBRATED)".format("NOT " if not cal else ""))
+        print("{}{}".format(res, "g" if cal else ""))
+
+finally:
+    d.close()
