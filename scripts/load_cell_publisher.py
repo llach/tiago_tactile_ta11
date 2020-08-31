3c18773b61cdb9d92b2fc83850179e49e518c0db
llach <llach@techfak.uni-bielefeld.de>
2020-08-31 15:34:47 +0200

    publish calibrated sensor values

--- scripts/load_cell_publisher.py
+++ scripts/load_cell_publisher.py
@@ -20,14 +20,36 @@ gainIndex = rospy.get_param("~gainIndex")
 settlingFactor = rospy.get_param("~settlingFactor")
 differential = rospy.get_param("~differential")
 
+right_m = rospy.get_param("~right_m")
+right_b = rospy.get_param("~right_b")
+
+left_m = rospy.get_param("~left_m")
+left_b = rospy.get_param("~left_b")
+
+print(
+    "\n~~~ TA11 Sensor Measurement ~~~\n\n" +
+    "Parameters:\n" +
+    "Scaling:\t {}\n".format(SCALING) +
+    "Smmothing:\t {}\n".format(SMOOTHING) +
+    "ResIndex\t {}\n".format(resolutionIndex) +
+    "Gain:\t\t {}\n".format(gainIndex) +
+    "Settling:\t {}\n".format(settlingFactor) +
+    "\n" +
+    "right_m:\t {}\n".format(right_m) +
+    "right_b:\t {}\n\n".format(right_b) +
+    "left_m:\t\t {}\n".format(left_m) +
+    "left_b:\t\t {}\n\n".format(left_b)
+)
+
+
 latest_values = [deque(maxlen=SMOOTHING) for _ in range(numChannels)]
 
 d = u6.U6()
 d.getCalibrationData()
 
 sensor_frames = [
-    "left",
-    "right"
+    "ta11_right_finger_link",
+    "ta11_left_finger_link"
 ]
 
 try:
@@ -55,8 +77,15 @@ try:
             latest_values[j].append(d.binaryToCalibratedAnalogVoltage(gainIndex, results[2 + j]) * SCALING)
 
         m = TA11()
+        m.header.frame_id = "base_link"
         m.header.stamp = rospy.Time.now()
-        m.sensor_values = [np.mean(dq) for dq in latest_values]
+
+        m.frame_names = sensor_frames
+        for i in range(len(sensor_frames)):
+            _m = right_m if i == 0 else left_m
+            _b = right_b if i == 0 else left_b
+
+            m.sensor_values.append(_m * np.mean(latest_values[i]) + _b)
 
         pub.publish(m)
 finally:
--- src/nodes/ta11_pub.cpp
+++ src/nodes/ta11_pub.cpp
@@ -11,7 +11,7 @@ int main (int argc, char** argv) {
   ros::AsyncSpinner spinner(4);
   spinner.start();
 
-  std::vector<std::string> sensor_frames = {"ta11_left_finger_link", "ta11_right_finger_link"};
+  std::vector<std::string> sensor_frames = {"ta11_right_finger_link", "ta11_left_finger_link"};
 
   tiago_tactile_ta11::TA11 t;
   std::thread th(&tiago_tactile_ta11::TA11::read_loop, std::ref(t));
@@ -20,6 +20,7 @@ int main (int argc, char** argv) {
 
   while (ros::ok()){
     tiago_tactile_msgs::TA11 tac;
+    tac.header.stamp = ros::Time::now();
     tac.header.frame_id = "base_link";
 
     tac.frame_names.push_back(sensor_frames[0]);
