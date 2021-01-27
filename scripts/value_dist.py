6e31485482dacad557bf12709ee7faec87e72629
llach <lachluca@gmail.com>
2021-01-27 14:53:04 +0100

    value dist done

--- scripts/value_dist.py
+++ scripts/value_dist.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-import rospy
-import u6
+import sys
 import time
+import rospy
 
 import numpy as np
 import seaborn as sns
@@ -10,9 +10,9 @@ import matplotlib.pyplot as plt
 
 from _collections import deque
 from tiago_tactile_msgs.msg import TA11
+sns.set(style="darkgrid")
 
-
-numSamples = 1000
+numSamples = 10000
 
 sides = {
     0: "right",
@@ -20,32 +20,38 @@ sides = {
 }
 
 start_t = None
-values = 2*[[]]
+values = [[], []]
 
-def plot_values():
-    global start_t
-    global values
-
-    print("took {:.2f} seconds".format(time.time() - start_t))
-    sns.distplot(values[0], hist=True, color='r', label='target values')
-    plt.show()
-
-    rospy.signal_shutdown("node done")
 
 def cb(vals):
     global values
     global numSamples
 
-    for i in range(2):
-        values[i].append(vals.sensor_values[i])
+    for i, v in enumerate(vals.sensor_values):
+        values[i].append(v)
 
     if len(values[0]) > numSamples:
-        sub.unregister()
-        plot_values()
+        rospy.signal_shutdown("got enough samples")
+
 
 rospy.init_node('value_dist')
 sub = rospy.Subscriber('ta11', TA11, callback=cb)
 start_t = time.time()
 
+print("collecting {} samples ...".format(numSamples))
+
 while not rospy.is_shutdown():
     rospy.spin()
+sub.unregister()
+
+print("took {:.2f} seconds\n".format(time.time() - start_t))
+for k, v in sides.items():
+    # sns.distplot(values[k], hist=True, color='b')
+    # plt.title(v)
+    # plt.show()
+
+    max_val = np.max(np.abs(values[k]))
+    mean = np.mean(values[k])
+    std = np.std(values[k])
+
+    print("{}\nmax = {:.5f}; mean = {:.5f}; std = {:.5f};\n".format(v, max_val, mean, std))
